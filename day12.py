816fcae94932d354c1b9a6c4c78e8d5bd46c7ad6
Luiz Scheidegger <luiz@Lindys-MacBook-Pro.local>
2025-12-18 16:55:44 -0800

    Day 12 working I think but too slow

--- day12.py
+++ day12.py
@@ -1,19 +1,24 @@
 from dataclasses import dataclass
+from functools import cache
 from typing import Generator, List, NewType, Tuple, Union
 import sys
 
 
+"""
+Encodes a 3x3 binary grid in the 9 least significant bits, followed by 6 bits of horizontal offset and 6 bits of vertical offset
+"""
 Present = NewType('Present', int)
 
 
 @dataclass(frozen = True)
 class Region:
     width: int
-    data: List[int]
+    data: Tuple[int]
+    area: int
 
     @staticmethod
     def from_width_height(width: int, height: int) -> 'Region':
-        return Region(width = width, data = [0] * height)
+        return Region(width = width, data = tuple([0] * height), area = width * height)
 
     def __repr__(self):
         result = ''
@@ -21,12 +26,15 @@ class Region:
             result += f'{row:0{self.width}b}'.replace('0', '.').replace('1', '#') + '\n'
         return result
 
-    def try_fit(self, present: Present, offset: Tuple[int, int]) -> 'Region':
+    @cache
+    def try_fit(self, present: Present) -> 'Region':
         """
         Try to fit the given present in the given region, with the optional given offset.
         The offset moves presents from top to bottom and *right to left*.
 
         Returns the new region, or same if it's not possible to fit."""
+        offset = (present >> 15) & (2**6 - 1), (present >> 9) & (2**6 - 1)
+        present &= 511
         if offset[0] + 3 > len(self.data):
             # hardcoded 3 because that's the height of a present and all presents in the input
             # fully need all 3 rows and columns
@@ -42,7 +50,7 @@ class Region:
                 return self
             result[offset[0] + idx] = present_row | region_row
     
-        return Region(self.width, result)  
+        return Region(self.width, tuple(result), self.area - present_area(present))
 
 
 def shape_str_to_present(shape_str: str) -> Present:
@@ -57,12 +65,24 @@ def shape_str_to_present(shape_str: str) -> Present:
 
 def present_to_shape_str(present: Present) -> str:
     result = ''
+    present &= 511
     while present > 0:
         result += '#' if present % 2 else '.'
-        present //= 2
+        present >>= 1
 
     result = '.' * (9 - len(result)) + ''.join(reversed(result))
     assert(len(result) == 9)
+    return result
+
+
+@cache
+def present_area(present: Present) -> int:
+    result = 0
+    present &= 511
+    while present > 0:
+        result += 1 if present % 2 else 0
+        present >>= 1
+
     return result
 
 
@@ -76,41 +96,64 @@ def shape_str_array_to_shape_str(shape_str_array: List[str]) -> str:
     return shape_str_array[0] + shape_str_array[1] + shape_str_array[2]
 
 
-def variants(present: Present) -> Generator[Present]:
+def region_variants(present: Present, region: Region) -> Generator[Present]:
+    assert(present < 512)
+
+    for x in range(region.width - 2):
+        for y in range(len(region.data) - 2):
+            yield (y << 15) | (x << 9) | present
+
+def variants(present: Present, region: Region) -> Generator[Present]:
+    assert(present < 512)
+    
+    # include self in variants
+    for p in region_variants(present, region):
+        yield p
+
     shape_str_array = shape_str_to_shape_str_array(present_to_shape_str(present))
 
     # horizontal flip
     temp = list(shape_str_array)
-    
     temp[0] = shape_str_array[0][2] + shape_str_array[0][1] + shape_str_array[0][0]
     temp[1] = shape_str_array[1][2] + shape_str_array[1][1] + shape_str_array[1][0]
     temp[2] = shape_str_array[2][2] + shape_str_array[2][1] + shape_str_array[2][0]
 
-    yield shape_str_to_present(shape_str_array_to_shape_str(temp))
+    present = shape_str_to_present(shape_str_array_to_shape_str(temp))
+    for p in region_variants(present, region):
+        yield p
     
     # vertical flip
-    yield shape_str_to_present(shape_str_array_to_shape_str([
+    present = shape_str_to_present(shape_str_array_to_shape_str([
         shape_str_array[2],
         shape_str_array[1],
         shape_str_array[0]]))
+
+    for p in region_variants(present, region):
+        yield p
 
     # 90 deg. rotation
     temp = [[shape_str_array[2][0], shape_str_array[1][0], shape_str_array[0][0]],
             [shape_str_array[2][1], shape_str_array[1][1], shape_str_array[0][1]],
             [shape_str_array[2][2], shape_str_array[1][2], shape_str_array[0][2]]]
-    yield shape_str_to_present(shape_str_array_to_shape_str(temp))
+    present = shape_str_to_present(shape_str_array_to_shape_str(temp))
+    for p in region_variants(present, region):
+        yield p
 
     # 180 deg. rotation
     temp = [[shape_str_array[2][2], shape_str_array[2][1], shape_str_array[2][0]],
             [shape_str_array[1][2], shape_str_array[1][1], shape_str_array[1][0]],
             [shape_str_array[0][2], shape_str_array[0][1], shape_str_array[0][0]]]
-    yield shape_str_to_present(shape_str_array_to_shape_str(temp))
+    present = shape_str_to_present(shape_str_array_to_shape_str(temp))
+    for p in region_variants(present, region):
+        yield p
 
     # 270 deg rotation
     temp = [[shape_str_array[0][2], shape_str_array[1][2], shape_str_array[2][2]],
             [shape_str_array[0][1], shape_str_array[1][1], shape_str_array[2][1]],
             [shape_str_array[0][0], shape_str_array[1][0], shape_str_array[2][0]]]
-    yield shape_str_to_present(shape_str_array_to_shape_str(temp))
+    present = shape_str_to_present(shape_str_array_to_shape_str(temp))
+    for p in region_variants(present, region):
+        yield p
 
 
 def print_present(present: Union[Present, str, List[str]]):
@@ -125,6 +168,11 @@ def print_present(present: Union[Present, str, List[str]]):
     for s in shape_str_array:
         print(s)
 
+    if isinstance(present, int):
+        x_offset = (present >> 9) & (2**6 - 1)
+        y_offset = (present >> 15) & (2**6 - 1)
+        print(f'{x_offset}x{y_offset}')
+
 
 def parse_input():
     presents = []
@@ -136,7 +184,7 @@ def parse_input():
             left, right = line.split(':')
             width, height = left.strip().split('x')
 
-            present_counts = [int(x) for x in right.strip().split()]
+            present_counts = tuple([int(x) for x in right.strip().split()])
             goal_states.append((Region.from_width_height(int(width), int(height)), present_counts))
 
         elif line == '':
@@ -155,7 +203,53 @@ def parse_input():
 def part1():
     data = parse_input()
 
-    
+    @cache
+    def can_fit(presents: Tuple[Present], goal_state: Tuple[Region, List[int]]) -> bool:
+        region, desired_presents = goal_state
+
+        if sum(desired_presents) == 0:
+            return True
+
+        # first non-zero present
+        present_idx = list(filter(lambda g: g[1] != 0, enumerate(desired_presents)))[0][0]
+
+        goal_state_without_present = tuple(
+            p - 1
+            if idx == present_idx
+            else p for (idx, p) in enumerate(desired_presents))
+
+        present = presents[present_idx]
+
+        if present_area(present) > region.area:
+            # Not enough room left, no need to bother trying
+            return False
+        
+        for variant in variants(present, region):
+            with_fit = region.try_fit(variant)
+            # print(with_fit)
+            if with_fit.data == region.data:
+                # This variant doesn't fit
+                continue
+            else:
+                # This variant of the present fits
+                if can_fit(presents, (with_fit, goal_state_without_present)):
+                    # And all others fit
+                    return True
+                else:
+                    # Strictly not needed, but indicates that we need to try another variant
+                    continue
+
+        # No variant of this present fits, so cut short
+        return False
+
+    result = 0
+    presents = tuple(data[0])
+
+    for goal_state in data[1]:
+        if can_fit(presents, goal_state):
+            result += 1
+
+    return result
 
 
 print(part1())
