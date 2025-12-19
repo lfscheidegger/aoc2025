14179e0774e6842abbdb63f34e0452e278ea7d4d
Luiz Scheidegger <luiz@Lindys-MacBook-Pro.local>
2025-12-19 10:45:04 -0800

    Day 9 part 2

--- day9.py
+++ day9.py
@@ -19,148 +19,84 @@ def part1():
     return distances[0][1]
 
 
-@dataclass(frozen=True)
-class Segment:
-    # x0, y0, x1, y1
-    data: Tuple[int, int, int, int]
+def input_to_svg():
+    lines = list(sys.stdin)
+    print("""<svg width="1000" height="1000" xmlns="http://www.w3.org/2000/svg">""")
 
-    @staticmethod
-    def from_tuple(data: Tuple[int, int, int, int]) -> 'Segment':
-        assert((data[0] == data[2]) or (data[1] == data[3]))
-        return Segment((
-            min(data[0], data[2]),
-            min(data[1], data[3]),
-            max(data[0], data[2]),
-            max(data[1], data[3])))
+    path_string = """<path stroke-width="0.1" stroke="black" fill="transparent" marker-start="url(#dot)" marker-mid="url(#dot)" marker-end="url(#dot)" d="M """
 
-    def __post_init__(self):
-        x0, y0, x1, y1 = self.data
-        if x0 != x1 and y0 != y1:
-            raise ValueError("Segment must be horizontal or vertical")
+    x, y = lines[0].split(",")
+    path_string += f"{int(x) / 100} {int(y) / 100}"
+    lines = lines[1:]
 
-        if x0 > x1 or y0 > y1:
-            raise ValueError("Segment in wrong order")
-
-    def direction(self) -> str:
-        if self.data[0] == self.data[2]:
-            return 'v'
-        return 'h'
-
-    def points(self) -> Generator[Tuple[int, int]]:
-        if self.direction() == 'v':
-            for y in range(self.data[1], self.data[3] + 1):
-                yield self.data[0], y
-        else:
-            for x in range(self.data[0], self.data[2] + 1):
-                yield x, self.data[1]
-
-    def contains_point(self, p: Tuple[int, int]) -> bool:
-        return p in self.points()
-
-    def ray_crosses(self, p: Tuple[int, int]) -> str:
-        """
-        Whether a ray starting at p inclusive, going to positive horizontal infinity, crosses this
-        segment. Returns n if the ray doesn't cross, y if it crosses, and i if the point is contained in
-        the segment."""
-        if self.contains_point(p):
-            return 'i'
-
-        if self.direction() == 'h':
-            # Collinear to the ray
-            return False
-
-        point_is_to_the_left = p[0] < self.data[0]
-        point_is_in_y_range = p[1] >= self.data[1] and p[1] <= self.data[3]
-
-        return 'y' if point_is_to_the_left and point_is_in_y_range else 'n'
-
-@dataclass(frozen = True)
-class Box:
-    data: Tuple[int, int, int, int]
-
-    def __post_init__(self):
-        x0, y0, x1, y1 = self.data
-
-        if x0 > x1 or y0 > y1:
-            raise ValueError("Segment in wrong order")
-
-    def subdivide(self) -> Generator['Box']:
-        x0, y0, x1, y1 = self.data
+    for line in lines:
+        x, y = line.split(",")
+        x, y = int(x), int(y)
+        path_string += f" L {x / 100} {y / 100}"
         
-        if x0 == x1 and y0 == y1:
-            raise ValueError('1x1 box cant be subdivided')
 
-        split_x = (x0 + x1) // 2
-        split_y = (y0 + y1) // 2
+    path_string += "\" />"
 
-        yield Box(x0, y0, split_x, split_y)
-        yield Box(split_x, y0, x1, split_y)
-        # yield Box(x0, split_y, split_x, 
+    print(path_string)
+    
+    print("""</svg>""")
 
-
-@dataclass(frozen = True)
-class Polygon:
-    segments: List[Segment]
-
-    @staticmethod
-    def from_input() -> 'Polygon':
-        lines = list(sys.stdin)
-        min_x, min_y = 2 ** 32, 2 ** 32
-        max_x, max_y = -2 ** 32, -2 ** 32
-        for line in lines:
-            x, y = line.split(",")
-            min_x = min(min_x, int(x))
-            min_y = min(min_y, int(y))
-
-            max_x = max(max_x, int(x))
-            max_y = max(max_y, int(y))
-        
-        lines = lines + [lines[0]]
-
-        segments = []
-        for p1, p2 in zip(lines, lines[1:]):
-            x, y = p1.split(",")
-            p1 = (int(x) - min_x, int(y) - min_y)
-            x, y = p2.split(",")
-            p2 = (int(x) - min_x, int(y) - min_y)
-            segments.append(Segment.from_tuple((p1[0], p1[1], p2[0], p2[1])))
-
-        return Polygon(segments)
-            
-    def contains(p: Tuple[int, int]) -> True:
-        """
-        Returns true iff the given point is inside the given polygon. Points on the edge
-        of the polygon count as inside."""
-        n_crosses = 0
-        for segment in self.segments:
-            crosses = segment.ray_crosses(p)
-            if crosses == 'i':
-                return True
-            elif crosses == 'y':
-                n_crosses += 1
-        return n_crosses % 2 == 1
 
 def part2():
-    p = Polygon.from_input()
+    """
+    This doesn't solve the general case but the actual shape is like a backwards pacman."""
+    coords = [(int(x), int(y)) for x, y in [l.strip().split(",") for l in sys.stdin]]
+
+    # The two "extreme points" to consider are one that's furthest right from its previous
+    # neighbor and then the one right after it
+    max_x_distance = max(enumerate(map(lambda c: c[1][0] - c[0][0], zip(coords, coords[1:]))), key=lambda v: v[1])
+    extremum_idx = max_x_distance[0] + 1
+    other_extremum_idx = extremum_idx + 1
+
+    rightest_x = coords[extremum_idx][0]
+
+    lowest_y = 0
+    # The len(coords) // 2 bullshit is to make sure I only look at points "late enough" in the circle
+    for neighbor, point in zip(coords[len(coords) // 2 + 1:], coords[len(coords) // 2:]):
+        if neighbor[0] > rightest_x and point[0] < rightest_x:
+            lowest_y = point[1]
+            break
+
+    bottom_left = None
+    for thing in enumerate(zip(coords[1:], coords)):
+        idx, (neighbor, point) = thing
+        if neighbor[1] < lowest_y and point[1] > lowest_y:
+            # The actual point is one prior to the point
+            bottom_left = coords[idx + 1 - 2]
+
+
+    top_right = coords[other_extremum_idx]
+    print(top_right, bottom_left)
+
+    return (1 + top_right[0] - bottom_left[0]) * (1 + top_right[1] - bottom_left[1])
     
-    #coords = [(int(x), int(y)) for x, y in [l.strip().split(",") for l in sys.stdin]]
+    # This is the "top half" rectangle, which appears to be the wrong one
+    #highest_y = 0
+    #for neighbor, point in zip(coords[1:], coords):
+    #    if neighbor[0] < rightest_x and point[0] > rightest_x:
+    #        highest_y = point[1]
+    #        break
+    
+    #top_left = None
+    #for thing in enumerate(zip(coords[1:], coords)):
+    #    idx, (neighbor, point) = thing
+    #    if neighbor[1] < highest_y and point[1] > highest_y:
+    #        top_left = coords[idx + 2]
+    #        break
 
-    #min_x = 2 ** 32
-    #max_x = - 2 ** 32
+    #bottom_right = coords[extremum_idx]
 
-    #min_y = min_x
-    #max_y = max_x
-
-    #for coord in coords:
-    #    min_x = min(min_x, coord[0])
-    #    max_x = max(max_x, coord[0])
-
-    #    min_y = min(min_y, coord[1])
-    #    max_y = max(max_y, coord[1])
-
-    #print(max_x - min_x, max_y - min_y)
+    #print(top_left, bottom_right)
+    #return (1 + bottom_right[0] - top_left[0]) * (1 + top_left[1] - bottom_right[1])
 
 
 print(part2())
+
+
 
 
